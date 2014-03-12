76bbcfcb101a83973618f3317a9d89bd7b744a51
Krishna Sundarram <krishna.sundarram@gmail.com>
2014-03-12 14:41:29 +0530

    Add UpSince() function to cpu.go

--- cpu.go
+++ cpu.go
@@ -16,12 +16,20 @@ var (
 func Uptime() (time.Duration, error){
 	bytes, err := ioutil.ReadFile(uptimeFile)
 	if err != nil {
-		return 0, errors.New("Could not read file")
+		return 0, errors.New("Could not read uptime file.")
 	}
 	uptime := strings.Split(string(bytes), " ")[0]
 	uptimeDuration, err := time.ParseDuration(uptime + "s")
 	if err != nil {
-		return 0, errors.New("Could not parse uptime")
+		return 0, errors.New("Could not parse uptime.")
 	}
 	return uptimeDuration, nil
+}
+
+func UpSince() (time.Time, error) {
+	duration, err := Uptime()
+	if err != nil {
+		return time.Unix(0, 0), errors.New("Could not get uptime.")
+	}
+	return time.Now().Add(-1 * duration), nil
 }
