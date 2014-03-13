7f5e9d0ca0851d2a63f4f8ae8cb13243b638c3ff
Krishna Sundarram <krishna.sundarram@gmail.com>
2014-03-13 11:08:49 +0530

    Add test for CPUUsage()

--- cpu_test.go
+++ cpu_test.go
@@ -3,6 +3,7 @@ package gotop
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestUptime(t *testing.T) {
@@ -21,4 +22,21 @@ func TestUpSince(t *testing.T) {
 		t.Fail()
 	}
 	fmt.Println(upSince)
+}
+
+func average(input []float64) float64 {
+	var sum float64
+	for i := 0; i < len(input); i++ {
+		sum = sum + input[i]
+	}
+	average := sum/float64(len(input))
+	return float64(int(average * 100)) / 100
+}
+
+func TestCPUUsage(t *testing.T) {
+	cpuStatChan := make(chan CPUStat)
+	go CPUUsage(cpuStatChan, 2*time.Second)
+	for cpuStats := range cpuStatChan {
+		fmt.Println(cpuStats.AverageUtilization, cpuStats.CPUUtilization, average(cpuStats.CPUUtilization))
+	}
 }
