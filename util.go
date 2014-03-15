9d1f6b4a227f6133ab8af8b32b1dd9e8cb563295
Krishna Sundarram <krishna.sundarram@gmail.com>
2014-03-16 00:13:06 +0530

    Increase testingDelay to 500ms
    Also change the error message returned by readFile().
    It now specified which file it failed to read.

--- util.go
+++ util.go
@@ -2,18 +2,20 @@ package gotop
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"time"
 )
 
 var (
-	testingDelay = time.Millisecond
+	testingDelay = 500 * time.Millisecond
 )
 
 func readFile(filename string) (string, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return "", errors.New("Could not read file.")
+		errorString := fmt.Sprintf("Could not read file %s .", filename)
+		return "", errors.New(errorString)
 	}
 	return string(bytes), nil
 }
