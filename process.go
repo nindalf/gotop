a343c68334bae5bbd0e9764fd88cb54cbe008b3c
Krishna Sundarram <krishna.sundarram@gmail.com>
2014-03-16 22:28:13 +0530

    Rewrite GetProcessData() in process.go
    Function now returns the CPU usage for each process.
    Memory is todo.

--- process.go
+++ process.go
@@ -13,33 +13,26 @@ import (
 var (
 	procDirectory   = "/proc"
 	processStatFile = "/proc/%d/stat"
+	processMemFile  = "/proc/%d/status"
 	processStates   = map[string]string{"R": "Running", "S": "Sleeping", "D": "Sleeping-uninterruptable", "Z": "Zombie", "T": "Traced/Stopped"}
 )
 
 type ProcessInfo struct {
-	Pid   int
-	Name  string
-	State string
+	Pid    int
+	Name   string
+	State  string
+	CPU    float64
+	Memory int
 }
 
-func readProcessStatFile(pid int) ([]string, error) {
-	path := fmt.Sprintf(processStatFile, pid)
-	file, err := readFile(path)
-	if err != nil {
-		return make([]string, 0), err
-	}
-	return strings.Split(file, " "), nil
-}
-
-func processInfo(pid int) (ProcessInfo, error) {
-	pfile, err := readProcessStatFile(pid)
-	if err != nil {
-		return ProcessInfo{}, err
-	}
-	name := pfile[1]
-	name = name[1 : len(name)-1] // Removes parentheses
-	state := processStates[pfile[2]]
-	return ProcessInfo{pid, name, state}, nil
+type pstat struct {
+	pid       int
+	name      string
+	state     string
+	utime     int
+	stime     int
+	startTime int
+	rss       int
 }
 
 func processIds() ([]int, error) {
@@ -61,8 +54,101 @@ func processIds() ([]int, error) {
 	return pids[:index], nil
 }
 
-func ProcessData(done <-chan struct{}, interval time.Duration) (<-chan []ProcessInfo, <-chan error) {
-	resultChan := make(chan []ProcessInfo)
+func readStatFile(pid int) ([]string, error) {
+	path := fmt.Sprintf(processStatFile, pid)
+	file, err := readFile(path)
+	if err != nil {
+		return make([]string, 0), err
+	}
+	return strings.Split(file, " "), nil
+}
+
+func processStat(pid int) (pstat, error) {
+	pfile, err := readStatFile(pid)
+	if err != nil {
+		return pstat{}, err
+	}
+	name := pfile[1]
+	name = name[1 : len(name)-1] // Removes parentheses
+	state := processStates[pfile[2]]
+	utime, _ := strconv.Atoi(pfile[13])
+	stime, _ := strconv.Atoi(pfile[14])
+	starttime, _ := strconv.Atoi(pfile[21])
+	rss, _ := strconv.Atoi(pfile[23])
+	return pstat{pid, name, state, utime, stime, starttime, rss}, nil
+}
+
+func calcCPU(prevps, curps pstat, prevtime, curtime int64) float64 {
+	// Time is in nanoseconds. Needs to be converted to jiffies.
+	prevjiffy, curjiffy := float64(prevtime)/10000000, float64(curtime)/10000000
+	usercpu := float64(curps.utime-prevps.utime) / (curjiffy - prevjiffy)
+	systemcpu := float64(curps.stime-prevps.stime) / (curjiffy - prevjiffy)
+	usage := (usercpu + systemcpu) * 100
+	// Return value is truncated to 2 places after decimal
+	return float64(int(usage * 100))/100
+}
+
+func processStats(done <-chan struct{}, delay time.Duration) (<-chan map[int]ProcessInfo, <-chan error) {
+	resultChan := make(chan map[int]ProcessInfo, 1)
+	errc := make(chan error, 1)
+	var err error
+	cleanup := func() {
+		errc <- err
+		close(errc)
+		close(resultChan)
+	}
+	go func() {
+		defer cleanup()
+		pids, err := processIds()
+		if err != nil {
+			return
+		}
+		var result map[int]ProcessInfo
+		prev := make(map[int]pstat)
+		var ps pstat
+		var cpu float64
+		var curtime, prevtime int64
+		for {
+			prevtime = curtime
+			time.Sleep(delay)
+			curtime = time.Now().UnixNano()
+			if err != nil {
+				return
+			}
+			result = make(map[int]ProcessInfo)
+			for _, pid := range pids {
+				ps, err = processStat(pid)
+				if err != nil {
+					return
+				}
+				if psprev, ok := prev[pid]; ok {
+					cpu = calcCPU(psprev, ps, prevtime, curtime)
+				} else {
+					cpu = 0
+				}
+				result[pid] = ProcessInfo{ps.pid, ps.name, ps.state, cpu, ps.rss/(256)}
+				if ps.pid == 20806 {
+					fmt.Println(ps.rss, float64(ps.rss)/(6.5*1024))
+				}
+				prev[pid] = ps
+			}
+
+			select {
+			case resultChan <- result:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return resultChan, errc
+}
+
+func processMem(pid int) (int, error) {
+	return 0, nil
+}
+
+func processMems(done <-chan struct{}, delay time.Duration) (<-chan map[int]int, <-chan error) {
+	resultChan := make(chan map[int]int, 1)
 	errc := make(chan error)
 	var err error
 	cleanup := func() {
@@ -72,28 +158,73 @@ func ProcessData(done <-chan struct{}, interval time.Duration) (<-chan []Process
 	}
 	go func() {
 		defer cleanup()
-		result := make([]ProcessInfo, 1000)
-		var index int
+		pids, err := processIds()
+		if err != nil {
+			return
+		}
+		var result map[int]int
+		var mem int
 		for {
-			pids, err := processIds()
-			if err != nil {
-				return
-			}
-			index = 0
+			result = make(map[int]int)
 			for _, pid := range pids {
-				pinfo, err := processInfo(pid)
+				mem, err = processMem(pid)
 				if err != nil {
 					return
 				}
-				result[index] = pinfo
-				index = index + 1
+				result[pid] = mem
 			}
 			select {
-			case resultChan <- result[:len(pids)]:
+			case resultChan <- result:
 			case <-done:
 				return
 			}
-			time.Sleep(interval)
+		}
+	}()
+	return resultChan, errc
+}
+
+func merge(pInfo map[int]ProcessInfo, pMem map[int]int) map[int]ProcessInfo {
+	for pid, val := range pInfo {
+		val.Memory = pMem[pid]
+		pInfo[pid] = val
+	}
+	return pInfo
+}
+
+func GetProcessInfo(done <-chan struct{}, delay time.Duration) (<-chan map[int]ProcessInfo, <-chan error) {
+	resultChan := make(chan map[int]ProcessInfo, 1)
+	errc := make(chan error)
+	pInfoChan, errsc := processStats(done, delay)
+	pMemChan, errmc := processMems(done, delay)
+	var err error
+	cleanup := func() {
+		errc <- err
+		close(errc)
+		close(resultChan)
+	}
+	go func() {
+		defer cleanup()
+		var pMem map[int]int
+		var pInfo map[int]ProcessInfo
+		for {
+			select {
+			case pInfo = <-pInfoChan:
+				pMem = <-pMemChan
+				pInfo = merge(pInfo, pMem)
+			case pMem = <-pMemChan:
+				pInfo = <-pInfoChan
+				pInfo = merge(pInfo, pMem)
+			case err = <-errsc:
+				return
+			case err = <-errmc:
+				return
+			}
+			select {
+			case resultChan <- pInfo:
+			case <-done:
+				return
+			}
+
 		}
 	}()
 	return resultChan, errc
