db2962d0019e468ffc0a291481c6ee73d468c9c4
Krishna Sundarram <krishna.sundarram@gmail.com>
2014-03-18 11:07:48 +0530

    Truncate value of net rate to 2 places after decimal

--- net.go
+++ net.go
@@ -58,7 +58,9 @@ func numbytes(path string) (float64, error) {
 func getrate(prevbytes, curbytes float64, prevtime, curtime int64) float64 {
 	// Needs to be converted from nanoseconds to seconds
 	timedelta := float64(curtime-prevtime) / 1000000000
-	return (curbytes - prevbytes) / (timedelta * 1024)
+	rate := (curbytes - prevbytes) / (timedelta * 1024)
+	// Return value is truncated to 2 places after decimal
+	return float64(int(100*rate)) / 100
 }
 
 func NetRate(done <-chan struct{}, delay time.Duration) (<-chan Net, <-chan error) {
